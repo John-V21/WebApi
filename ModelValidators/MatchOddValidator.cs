bb67ff2493da320ac2c37d701e355b2fdd2403c7
Giannis Vossos <g.vossos@atc.gr>
2021-04-07 19:25:03 +0300

    Added Model Validators

---
+++ ModelValidators/MatchOddValidator.cs
@@ -0,0 +1,19 @@
+﻿using Accepted.Models;
+using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Accepted.ModelValidators
+{
+    public class MatchOddValidator : AbstractValidator<MatchOdd>
+    {
+        public MatchOddValidator()
+        {
+            RuleFor(x => x.Specifier).NotNull().MinimumLength(1).MaximumLength(1).WithMessage("One char is required of 1,2 or X");
+            RuleFor(x => x.Odd).GreaterThan(0).WithMessage("Odd must be greater than 0");
+        }
+    }
+}
---
+++ ModelValidators/MatchValidator.cs
@@ -0,0 +1,15 @@
+﻿using Accepted.Models;
+using FluentValidation;
+using System;
+
+namespace Accepted.ModelValidators
+{
+    public class MatchValidator : AbstractValidator<Match>
+    {
+        public MatchValidator()
+        {
+            RuleFor(x => x.TeamA).Must((model, field) => model.TeamA != model.TeamB).WithMessage("TeamA and TeamB cannot be equal");
+            RuleFor(x => x.MatchTime).InclusiveBetween(TimeSpan.FromHours(0), TimeSpan.FromHours(24)).WithMessage("MatchTime out of range 00-24");
+        }
+    }
+}
